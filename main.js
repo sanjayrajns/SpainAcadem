7e7df75a7b3095600f0de6df56b5ce29ce0fae94
Sanjay Raj <sanjayrajns07@gmail.com>
2025-08-16 19:08:31 +0530

    updated

--- main.js
+++ main.js
@@ -5,9 +5,21 @@
 
 const accordionHeaders = document.querySelectorAll('.accordion-header');
 
+accordionHeaders.forEach(header => {
+    header.addEventListener('click', () => {
+        const content = header.nextElementSibling;
+        header.classList.toggle('active');
 
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+});
 
-// gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
+
 const hamburger = document.getElementById('hamburger');
 const navMobile = document.getElementById('navMobile');
 const overlay = document.getElementById('overlay');
